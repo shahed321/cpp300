b2210ca9121ec361ea771a67562412cdacae5aab
Shahed Ahmed Akkagi <sabuj.321@outlook.com>
2022-09-15 00:43:03 +0600

    cout_001

---
+++ cout/cout_001.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //to print output to stdout using cout and to end a line using endl
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
